b5b6dcffc61af5d999daec83587468daca1ac754
Mikhail <85116490+MishaZhem@users.noreply.github.com>
2026-03-13 13:39:14 +0100

    feat: add layout component

---
+++ Migration-Kotlin/app/components/layout/index.tsx
@@ -0,0 +1,19 @@
+import "./index.scss";
+
+export function Section({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) {
+    return (
+        <section className={"kto-layout-section" + (className ? " " + className : "")}>
+            {children}
+        </section>
+    );
+}
+
+export function Container({ children }: { children: React.ReactNode }) {
+    return <div className="kto-layout-container">{children}</div>;
+}
