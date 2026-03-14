08ff3932416cbaadcebeae5f2329f76398913bce
Mikhail <85116490+MishaZhem@users.noreply.github.com>
2026-03-14 11:52:41 +0100

    feat: add latest from Kotlin section

--- Migration-Kotlin/app/pages/index/overview-page.tsx
+++ Migration-Kotlin/app/pages/index/overview-page.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@rescui/ui-contexts";
 import { HeaderSection } from "./header-section";
+import { LatestFromKotlinSection } from "./latest-from-kotlin-section";
 
 export default function OverviewPage() {
     return (
         <ThemeProvider theme="dark">
             <main className="overview-page">
                 <HeaderSection />
+                <LatestFromKotlinSection />
             </main>
         </ThemeProvider>
     );
